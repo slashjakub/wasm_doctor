55a993dac0609f1da7e2dc2245c6bafe76a8be18
Jakub Mares <maresj39@fit.cvut.cz>
2024-03-10 22:53:12 +0100

    Register memory copy and fill (not properly)

--- lib/insn_impl_fc.h
+++ lib/insn_impl_fc.h
@@ -92,6 +92,11 @@ retry:
                         goto retry;
                 }
                 memmove(dst_p, src_p, n);
+
+                // TODO: handle validity properly
+                register_store(val_d.u.i32, val_n.u.i32);
+                printf("memory of size %u copied at addr %10u\n", val_n.u.i32,
+                       val_d.u.i32);
         }
         SAVE_PC;
         INSN_SUCCESS;
@@ -118,6 +123,11 @@ INSN_IMPL(memory_fill)
                         goto fail;
                 }
                 memset(vp, (uint8_t)val_val.u.i32, n);
+
+                // TODO: handle validity properly
+                register_store(val_d.u.i32, val_n.u.i32);
+                printf("memory of size %u filled with %u at addr %10u\n",
+                       val_n.u.i32, val_val.u.i32, val_d.u.i32);
         }
         SAVE_PC;
         INSN_SUCCESS;
